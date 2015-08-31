544a31f4f3916a6ca3d1c9ddac2afc3aa0866089
ramsundark5 <ramsundark5@gmail.com>
2015-08-31 15:12:37 -0400

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ fs = require('graceful-fs'),
 gutil = require('gulp-util'),
 map = require('map-stream'),
 tempWrite = require('temp-write'),
-cachebust = require('cachebust');
+cachebust = require('cachebustrel');
 
 module.exports = function (options) {
 	return map(function (file, cb) {
